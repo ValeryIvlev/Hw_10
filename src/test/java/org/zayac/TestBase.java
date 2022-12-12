d2c88b34f5c576123a41a24c29c42fecc4f0b338
Valery <qaivlev@yandex.ru>
2022-12-12 11:35:34 +0300

    Added Test

---
+++ src/test/java/org/zayac/TestBase.java
@@ -0,0 +1,34 @@
+package org.zayac;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.codeborne.selenide.Selenide.webdriver;
+import static io.qameta.allure.Allure.attachment;
+
+public class TestBase {
+
+
+    @BeforeAll
+    static void BeforeAll() {
+
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+
+    }
+
+    @BeforeEach
+    void setUp() {
+
+       Attachments.screenshotAs("Last screenshot");
+       Attachments.addVideo();
+       Attachments.browserConsoleLogs();
+       Attachments.pageSource();
+
+
+    }
+
+}
+
---
+++ src/test/java/org/zayac/TestUserForm.java
@@ -0,0 +1,59 @@
+package org.zayac;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class TestUserForm {
+
+    @Test
+    void testFrom() {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открытие сайта", () -> {
+                    open("https://demoqa.com/automation-practice-form");
+                    executeJavaScript("$('#fixedban').remove()");
+                    executeJavaScript("$('footer').remove()");
+                });
+
+        step("Заполнение формы", () -> {
+        $("#firstName").setValue("Ivan");
+        $("#lastName").setValue("Ivanov");
+        $("#userEmail").setValue("Mail@mail.ru");
+        $("#gender-radio-1").parent().click();
+        $("#userNumber").setValue("7999111123");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("1993");
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("July");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__day--004").click();
+        $("#subjectsInput").setValue("Economics").pressEnter();
+        $("#hobbies-checkbox-1").parent().click();
+        $("#uploadPicture").uploadFromClasspath("file.png");
+        $("#currentAddress").setValue("Subject one or no one:)");
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Noida").pressEnter();
+        $("#submit").click();
+        });
+
+        step("Проверка заполнения формы", () -> {
+        $(".table-responsive").shouldHave(text("Ivan Ivanov"), text("Mail@mail.ru"), text("male"), text("7999111123"),
+                text("04 July,1993"), text("Economics"), text("Sports"), text("file.png"), text("Subject one or no one:)"),
+                text("NCR Noida"));
+        $("#closeLargeModal").click();
+        });
+    }
+
+}
